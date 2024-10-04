d289f59e522c0d6c002c2e1f8d1c5224be878c8b
xxzn <mrq0218@163.com>
2024-10-04 18:52:43 +0800

    delete a at the 37th line

--- test5.py
+++ test5.py
@@ -34,5 +34,5 @@ for requested_topping in requested_toppings:
     if requested_topping in available_toppings:
         print("Adding " + requested_topping + ".")
     else:
-        print("we do not have a "+requested_topping+".")
+        print("we do not have "+requested_topping+".")
         print("在dev分支中加入一行")
