a3cc8d6b6ca56437711817386e1b1a748115bb5c
xxzn <mrq0218@163.com>
2024-10-13 21:01:55 +0800

    continue to learnning the matplotlib and pygal

---
+++ matplotlib3.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from random_walk import RandmWalk
+
+while True:
+    rw = RandmWalk()
+    rw.fill_walk()
+
+    # 创建列表，用于颜色映射
+    point_list = list(range(rw.num_points))
+
+    # pyplot显示用法
+    # The "Axes" interface is how Matplotlib is implemented, 
+    # and many customizations and fine-tuning end up being done at this level.
+    fig = plt.figure(figsize=(6,4))
+    randm_fig = fig.subplots() #返回一个Axes实例
+
+    # 绘制图像
+    randm_fig.scatter(rw.x_values, rw.y_values, c=point_list, cmap=plt.cm.Blues, 
+                edgecolors="none",s=15)
+    # 突出起点和终点
+    randm_fig.scatter(0, 0, c='green', edgecolors='none', s=100)
+    randm_fig.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+                s=100)
+    
+
+    plt.show()
+
+    keep_running = input("Make another walk?(y/n):")
+    if keep_running == 'n':
+        break
+
+    """
+    此外可以修改点数：rw = RandmWalk(50000)
+    """
---
+++ pygal1.py
@@ -0,0 +1,30 @@
+from die import Die
+import pygal
+
+# 创建两个D6骰子
+die_1 = Die() # 可以更改骰子的面数，如die_1 = Die(10)
+die_2 = Die()
+results = []
+
+# 存储结果
+for roll_num in range(1000):
+    results.append(die_1.roll() + die_2.roll())
+
+# 计算各点出现的次数
+frequencies = []
+min_result = 2
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(min_result, max_result+1):
+    frequencie = results.count(value)
+    frequencies.append(frequencie)
+
+# 对结果进行可视化
+hist = pygal.Bar() #创建一个Bar实例
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = list(range(min_result,max_result+1))
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add("D6 + D6", frequencies) # 添加标签和值
+hist.render_to_file("dice_visual.svg") # 保存为svg文件
